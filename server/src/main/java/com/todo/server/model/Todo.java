6eeb25899ed3e7d115fc620b2f9c331f29d219dd
Rohith56 <121215281+Rohith56@users.noreply.github.com>
2025-05-22 22:51:48 +0530

    Update Todo.java

---
+++ server/src/main/java/com/todo/server/model/Todo.java
@@ -0,0 +1,19 @@
+package com.todo.server.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private boolean completed;
+}
